f0741a2dc9723efe4f0e3746cac7c733928b23f6
qinyuanmao <qinyuanmao.live@gmail.com>
2014-11-04 15:18:50 +0800

    个人信息界面草图

--- app/src/main/java/com/peoleo/main/Fragment/UserFragment.java
+++ app/src/main/java/com/peoleo/main/Fragment/UserFragment.java
@@ -1,9 +1,22 @@
 package com.peoleo.main.Fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.peoleo.main.R;
 
 /**
  * Created by hu on 14-11-4.
  */
 public class UserFragment extends Fragment {
+    private View parentView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        parentView = inflater.inflate(R.layout.fragment_user,container,false);
+        return parentView;
+    }
 }
